d10466773f15a3bbf3733d919f569f13ddcd9287
Kynderman Baraldi <kynderman@gmail.com>
2023-05-15 19:54:48 -0300

    deploy

--- app/src/routes/app-routes.ts
+++ app/src/routes/app-routes.ts
@@ -14,6 +14,8 @@ export const APP_ROUTES = {
         tips: {
             codigo_oab: '/tips/codigo-oab',
             zonas: '/tips/zonas',
+            entes_municipais: '/tips/entes-municipais',
+            entes_estaduais: '/tips/entes-estaduais',
         }
     },
 };
@@ -26,26 +28,44 @@ export const menuItems = [
       path: APP_ROUTES.public.home,
     },
     {
-      
       title: 'Certificador',
       dropdown: [
-        {
-          
+        { 
           title: 'Certificar prazo',
           path: APP_ROUTES.private.servicos.certificar_prazo,
         },
         {
-          
           title: 'Certidão de Honorários OAB',
           path: APP_ROUTES.private.servicos.certitidao_honorarios_oab,
         },
       ],
     },
     {
+        title: 'Dicas',
+        dropdown: [
+          {
+            title: 'Código defensoria',
+            path: APP_ROUTES.public.tips.codigo_oab,
+          },
+          {
+            
+            title: 'Zonas',
+            path: APP_ROUTES.public.tips.zonas,
+          },
+            {
+                title: 'Entes municipais',
+                path: APP_ROUTES.public.tips.entes_municipais,
+            },
+            {
+                title: 'Entes estaduais',
+                path: APP_ROUTES.public.tips.entes_estaduais,
+            },
+        ],
+      },
+    {
       title: 'Sobre',
       path: APP_ROUTES.public.sobre,
     },
-    
   ];
 
 export function checkIsPublicRoute(rota: string): boolean {

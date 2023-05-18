b3b771a65e532ddf5047fe86ef8b5f80a391d0d6
Kynderman Baraldi <kynderman@gmail.com>
2023-05-18 13:28:54 -0300

    Add api url in .env

--- app/src/pages/servicos/certitidao-honorarios-oab.tsx
+++ app/src/pages/servicos/certitidao-honorarios-oab.tsx
@@ -46,7 +46,7 @@ export default function CertidaoOAB() {
       body: formData
     };
 
-    const response = await fetch('http://168.75.76.91:5001/certidao-oab', options)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}certidao-oab`, options)
     const data = await response.json()
     setCert(data)
     if (inputfileRef.current) {
